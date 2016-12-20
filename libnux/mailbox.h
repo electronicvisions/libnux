5adbb2a9d974bfbd1a403fd3dcbbbebec1330660
David Stöckel <david.stoeckel@kip.uni-heidelberg.de>
2016-12-20 09:29:32 +0100

    Add function to write strings to mailbox
    Change-Id: Ifdad514b714451e89ff109d56bebc86b1a1a65a4

--- libnux/mailbox.h
+++ libnux/mailbox.h
@@ -7,3 +7,6 @@ extern uint8_t mailbox_end;
 
 uint32_t mailbox_write(uint32_t const offset, uint8_t const * src, uint32_t const size);
 uint32_t mailbox_read(uint8_t * dest, uint32_t const offset, uint32_t const size);
+
+uint32_t libnux_mailbox_write_string(char const * str);
+uint32_t libnux_mailbox_write_int(uint32_t const n);
--- src/mailbox.c
+++ src/mailbox.c
@@ -1,5 +1,30 @@
 #include "libnux/mailbox.h"
 
+static uint32_t write_head_offset = 0;
+
+char* itoa(uint32_t value, uint32_t const base) {
+	static char buffer[32] = {0};
+
+	/* Check for valid base */
+	if((base > 16) || (base < 2)) {
+		/* TODO set errno EDOM as soon as implemented */
+		buffer[0] = 0;
+		return buffer;
+	}
+
+	/* Number system conversion */
+	uint32_t index = 0;
+	do {
+		buffer[index] = "0123456789abcdef"[value % base];
+		index++;
+		value /= base;
+	} while (value != 0);
+
+	/* Return null terminated string */
+	buffer[index] = 0;
+	return buffer;
+}
+
 uint32_t mailbox_write(uint32_t const offset, uint8_t const * src, uint32_t const size) {
 	volatile uint8_t* ptr = &mailbox_base + offset;
 	uint32_t i;
@@ -20,4 +45,20 @@ uint32_t mailbox_read(uint8_t * dest, uint32_t const offset, uint32_t const size
 	}
 
 	return ptr - &mailbox_base + offset;
+}
+
+uint32_t libnux_mailbox_write_string(char const * str) {
+	char const * ptr = str;
+	for (; (*ptr != 0); write_head_offset++, ptr++) {
+		if (&mailbox_base + write_head_offset == &mailbox_end) {
+			/* TODO set errno ERANGE as soon as implemented */
+			return ptr - str;
+		}
+		*(&mailbox_base + write_head_offset) = *ptr;
+	}
+	return ptr - str;
+}
+
+uint32_t libnux_mailbox_write_int(uint32_t const n) {
+	return libnux_mailbox_write_string(itoa(n, 10));
 }
