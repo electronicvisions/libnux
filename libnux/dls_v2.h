0004796d1d9180c8277cdae717cbf5827b65decf
David Stöckel <david.stoeckel@kip.uni-heidelberg.de>
2017-03-08 11:51:28 +0100

    Add rate counter and synapse driver omnibus address to dls v2 constants
    Change-Id: I280296f2a8a5b7ea0c7fcb7793e75c5adabe5455

--- libnux/dls_v2.h
+++ libnux/dls_v2.h
@@ -21,3 +21,21 @@ uint32_t const dls_vreset_acausal_base = 0xd000;
 /* Bitmask for weight decoding */
 uint32_t const dls_weight_mask = 0x3f;
 uint32_t const dls_decoder_mask = 0x3f;
+
+/* Omnibus addresses
+ * The omnibus addressing from the PPU is shifted by 2 bits compared to the
+ * addressing from the FPGA, plus setting the most significant bit. For the
+ * addresses refer to Hartel 2017, 6.2.1. The MSB selects between addressing
+ * the SRAM or the bus.
+ * Reading and writing to the omnibus should be done by dereferencing 32 bit
+ * pointers. Therefore, all omnibus addresses should be placed here in the
+ * format
+ *
+ * uint32_t* const dls_module_base = (uint32_t*)((address << 2) | (1 << 31));
+ *
+ * */
+
+/* Addressing of rate counters */
+uint32_t* const dls_rates_base = (uint32_t*)((0x1e000000 << 2) | (1 << 31));
+/* Address of synapse driver configuration */
+uint32_t* const dls_syndrv_base = (uint32_t*)((0x1c000000 << 2) | (1 << 31));
