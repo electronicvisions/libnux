0e6c30a4c8b5b9b033216ab5d4608c674b7ef0ae
timow <timow@kip.uni-heidelberg.de>
2018-05-22 13:10:50 +0200

    Re-add formerly deprecated PRNG
    Change-Id: If7f9287b41865a3278c16a0e875f080b2ab04304

--- libnux/random.c
+++ libnux/random.c
@@ -39,3 +39,10 @@ void xorshift_vector(const vector uint8_t* seed)
 	*(x + 2) = *(x + 3);
 	*(x + 3) ^= (*(x + 3) >> 19) ^ t ^ (t >> 8);
 }
+
+uint32_t random_lcg(uint32_t *seed) {
+  // constants from Numerical Recipes via Wikipedia
+  uint32_t rv = 1664525 * (*seed) + 1013904223;
+  *seed = rv;
+  return rv;
+}
--- libnux/random.h
+++ libnux/random.h
@@ -6,3 +6,4 @@
 uint32_t xorshift32(uint32_t* seed);
 uint32_t draw_poisson(uint32_t* seed, uint32_t cutoff, uint32_t dt);
 void xorshift_vector(const vector uint8_t* seed);
+uint32_t random_lcg(uint32_t *seed);
