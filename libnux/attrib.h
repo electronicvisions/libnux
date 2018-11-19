6919869901839e98b11dde3c1226b521dba560b1
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2018-11-19 22:24:47 +0100

    Add inline attributes
    Change-Id: Ib24403025314a169cf3ce06c816b79e996297f5f

--- libnux/attrib.h
+++ libnux/attrib.h
@@ -2,4 +2,5 @@
 
 #define ATTRIB_UNUSED __attribute__((unused))
 #define ATTRIB_ALIGN_WORD __attribute__((aligned(4)))
-
+#define ATTRIB_ALWAYS_INLINE inline __attribute__((always_inline))
+#define ATTRIB_NOINLINE __attribute__((noinline))
