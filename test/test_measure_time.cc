b922e09723b43ed7add7ff4943a5c32f815bb9f9
Yannik Stradmann <yannik.stradmann@kip.uni-heidelberg.de>
2018-12-17 15:36:52 +0100

    Robustify time measurement test
    * Reduce influence of measurement pro-/epilogue by running the critical
      section multiple times
    * Keep test time constant by decreasing iterations for long sleep tests
    * Increse valid sleep measurement interval to cope with the increased
      runtime due to stack protection/redzone checks.
    
    Change-Id: I4331afe4b56b2b50d909b311a47f56ed16c53d10

--- test/test_measure_time.cc
+++ test/test_measure_time.cc
@@ -2,16 +2,26 @@
 extern "C" {
 #include "libnux/unittest.h"
 }
-#include "libnux/time.hpp"
 #include "libnux/helpers.hpp"
+#include "libnux/time.hpp"
 
 template <size_t N>
 void test_sleep_cycles(char const* msg, times<N>& t, time_base_t sleep_time, time_base_t max_time)
 {
+	constexpr int inner_loop_count = 100;
+	time_base_t post_loop;
+
 	for (auto& pair : t) {
 		pair.first = now();
-		sleep_cycles(sleep_time);
-		pair.second = now();
+
+		/* Run the critical section multiple times to reduce the influence of
+		   start/stop overhead. */
+		for (int i = 0; i <= inner_loop_count; i++) {
+			sleep_cycles(sleep_time);
+		}
+
+		post_loop = now();
+		pair.second = pair.first + (post_loop - pair.first) / inner_loop_count;
 	}
 	print_measurement(msg, t);
 	libnux_test_true(min(t) > sleep_time);
@@ -57,31 +67,31 @@ extern "C" int start()
 	time_pair global;
 	global.first = now();
 
-	times<10> t;
+	times<10> tentimes;
+	times<1> onetime;
 	/* Empty measurement using example measurement function. */
 	libnux_testcase_begin("empty");
-	measure_empty(t);
-	libnux_test_true(min(t) > 0);
+	measure_empty(tentimes);
+	libnux_test_true(min(tentimes) > 0);
 	libnux_testcase_end();
 
 	/* Measure sleep_cycles with different cycles. */
 	libnux_testcase_begin("sleep_cycles");
-	test_sleep_cycles("sleep_cycles 10", t, 10, 400);
-	test_sleep_cycles("sleep_cycles 100", t, 100, 400);
-	test_sleep_cycles("sleep_cycles 1000", t, 1000, 1300);
-	test_sleep_cycles("sleep_cycles 10000", t, 10000, 10300);
-	test_sleep_cycles("sleep_cycles 100000", t, 100000, 100300);
+	test_sleep_cycles("sleep_cycles 10", tentimes, 10, 600);
+	test_sleep_cycles("sleep_cycles 100", onetime, 100, 600);
+	test_sleep_cycles("sleep_cycles 1000", onetime, 1000, 1600);
+	test_sleep_cycles("sleep_cycles 10000", onetime, 10000, 10600);
 	libnux_testcase_end();
 
 	libnux_testcase_begin("optimization");
 	/* Measure multiplication of two integer literals
 	   without prohibiting precalculation. */
-	measure_int_multiply("int operator*", t);
-	auto min_optimized_away = min(t);
+	measure_int_multiply("int operator*", tentimes);
+	auto min_optimized_away = min(tentimes);
 	/* Measure multiplication of two integer literals
 	   and have them not optimized away, i.e. precalculated. */
-	measure_int_multiply_not_optimized_away("int operator* not optimized away", t);
-	auto min_not_optimized_away = min(t);
+	measure_int_multiply_not_optimized_away("int operator* not optimized away", tentimes);
+	auto min_not_optimized_away = min(tentimes);
 	libnux_test_true(min_optimized_away < min_not_optimized_away);
 	libnux_testcase_end();
 
