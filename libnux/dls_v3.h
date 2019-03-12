6d2f895103d21b8f4d91fe5e39246449fa70018f
Aron Leibfried <aron.leibfried@kip.uni-heidelberg.de>
2019-03-12 12:54:58 +0100

    Add driver amount and some addresses for DLSv3
    Change-Id: I443e61813b0eae2c50651e7744a9848cc152caea

--- libnux/dls_v3.h
+++ libnux/dls_v3.h
@@ -11,6 +11,9 @@ static uint32_t const dls_num_synapses = 32 * 32;
 /* Vector addressing of synapses */
 static uint32_t const dls_num_synapse_vectors = 32 * 32 / 16;
 
+/* Amount of synapse drivers */
+static uint32_t const amount_synapse_drivers = 32 / 2;
+
 /* Addressing for vector in/out */
 static uint32_t const dls_weight_base = 0x0000;
 static uint32_t const dls_decoder_base = 0x4000;
@@ -50,6 +53,10 @@ static uint32_t* const dls_rates_reset =
 
 /* Address of synapse driver configuration */
 static uint32_t* const dls_syndrv_base = (uint32_t*) ((0x14000000ul << 2) | (1ul << 31));
+static uint32_t* const dls_syndrv_config_base =
+    (uint32_t*) (((0x14000000ul | (1ul << 11)) << 2) | (1ul << 31));
+static uint32_t* const dls_padi_fire =
+    (uint32_t*) (((0x14000000ul | (1ul << 8)) << 2) | (1ul << 31));
 
 /* Address of capacitive memory configuration */
 static uint32_t* const dls_capmem_config_base = (uint32_t*) ((0x10010000ul << 2) | (1ul << 31));
@@ -58,5 +65,14 @@ static uint32_t* const dls_capmem_cell_base = (uint32_t*) ((0x10000000ul << 2) |
 /* Address for spike injection */
 static uint32_t* const dls_spike_base = (uint32_t*) ((0x15000000ul << 2) | (1ul << 31));
 
-/* FIXME: What is this for */
+/* Address of digital neuron configuration for triggering reset */
 static uint32_t* const dls_nrn_digital_mod = (uint32_t*) ((0x12000000ul << 2) | (1ul << 31));
+
+/* Address of digital neuron configuration */
+static uint32_t* const dls_nrn_digital_set =
+    (uint32_t*) (((0x12000000ul | (1ul << 12)) << 2) | (1ul << 31));
+static uint32_t* const dls_nrn_digital_clock_scale =
+     (uint32_t*) (((0x12001000ul | (1ul << 11)) << 2) | (1ul << 31));
+
+/* Address of synapse array configuration */
+static uint32_t* const dls_synapse_array_base = (uint32_t*) ((0x0001f000ul << 2) | (1ul << 31));
