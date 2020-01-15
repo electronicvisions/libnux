80b2606d2159ef0e417a8839a6481c33a67b78b1
Aron Leibfried <aron.leibfried@kip.uni-heidelberg.de>
2020-01-15 13:24:23 +0100

    Add correct addressing for vector in/out for HX
    Change-Id: Iea5c9d65ff51b12d8b6f24131dc132dda654baff

--- libnux/dls_vx.h
+++ libnux/dls_vx.h
@@ -11,12 +11,16 @@ static uint32_t const dls_num_synapses = 32 * 32;
 static uint32_t const dls_num_synapse_vectors = 32 * 32 / 16;
 
 /* Addressing for vector in/out */
-static uint32_t const dls_weight_base = 0x0000;
-static uint32_t const dls_decoder_base = 0x4000;
-static uint32_t const dls_causal_base = 0x8000;
-static uint32_t const dls_vreset_causal_base = 0x9000;
-static uint32_t const dls_acausal_base = 0xc000;
-static uint32_t const dls_vreset_acausal_base = 0xd000;
+static uint32_t const dls_weight_base         = 0x00000000;
+static uint32_t const dls_decoder_base        = 0x00040000;
+static uint32_t const dls_causal_base         = 0x00080000;
+static uint32_t const dls_vreset_causal_base  = 0x00090000;
+static uint32_t const dls_acausal_base        = 0x000c0000;
+static uint32_t const dls_vreset_acausal_base = 0x000d0000;
+static uint32_t const dls_config_even_base    = 0x00010000;
+static uint32_t const dls_config_odd_base     = 0x00020000;
+static uint32_t const dls_raw_base            = 0x000f0000;
+static uint32_t const dls_randgen_base        = 0x000e0000;
 
 /* Byte to be written to synapse for correlation reset */
 static uint8_t const dls_correlation_reset = 0x3;
