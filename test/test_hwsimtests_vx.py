c1ff9e9188f45187b83386a1db57371c81836150
Yannik Stradmann <yannik.stradmann@kip.uni-heidelberg.de>
2020-02-11 18:05:42 +0000

    Randomly choose PPU per libnux test case
    Depends-On: 9527
    Change-Id: I0c3d1f35ffa0672ee806f47b5272de0e39e6935f

--- test/test_hwsimtests_vx.py
+++ test/test_hwsimtests_vx.py
@@ -1,9 +1,12 @@
+import random
 import unittest
 from typing import Set
 
 from dlens_vx.sta import PlaybackProgramExecutor, generate, DigitalInit
+from dlens_vx.halco import PPUOnDLS, iter_all
 from dlens_vx.tools.run_ppu_program import load_and_start_program, \
     stop_program, wait_until_ppu_finished, PPUTimeoutError
+from dlens_vx import logger
 from helpers.hwtest_common import get_special_binaries, find_binaries, \
     PpuHwTest
 
@@ -34,16 +37,21 @@ class LibnuxHwSimTestsVx(unittest.TestCase):
         for test in cls.TESTS:
             def generate_test(ppu_test: PpuHwTest):
                 def test_func(self: LibnuxHwSimTestsVx):
-                    load_and_start_program(cls.EXECUTOR, ppu_test.path)
+                    log = logger.get("LibnuxHwSimTestsVx.%s" % ppu_test.name)
+                    ppu = random.choice(list(iter_all(PPUOnDLS)))
+                    log.info("Running test on %s." % ppu)
+
+                    load_and_start_program(cls.EXECUTOR, ppu_test.path, ppu)
                     try:
-                        wait_until_ppu_finished(cls.EXECUTOR, timeout=int(5e5))
+                        wait_until_ppu_finished(cls.EXECUTOR, timeout=int(5e5),
+                                                ppu=ppu)
                         self.assertFalse(ppu_test.expect_timeout,
                                          "Expected timeout did not happen.")
                     except PPUTimeoutError:
                         self.assertTrue(ppu_test.expect_timeout,
                                         "Unexpected timeout.")
                     finally:
-                        returncode = stop_program(cls.EXECUTOR)
+                        returncode = stop_program(cls.EXECUTOR, ppu=ppu)
 
                     self.assertEqual(ppu_test.expected_exit_code, returncode,
                                      f"Exit code was {returncode}, expected "
