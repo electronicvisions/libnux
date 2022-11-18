2c1eb65f76d10e142c1215d7922b1b0bb34fe6f9
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2022-11-18 14:00:57 +0100

    Reduce vector frac sat 8 1 test runtime
    * don't test equality of all elements with EXPECT_EQ, but test
      equality of all after loop with EXPECT_TRUE
    
    Change-Id: Ic2e176309dfda609666eb2b0fe8d5400d07ed624

--- tests/hw/libnux/vx/test_vector_frac_sat_8_1.cpp
+++ tests/hw/libnux/vx/test_vector_frac_sat_8_1.cpp
@@ -7,14 +7,22 @@ TEST(VectorFracSat8, Construction)
 	[[maybe_unused]] VectorFracSat8 from_nothing;
 
 	VectorFracSat8 from_int(12);
-	for (size_t i = 0; i < from_int.size; ++i) {
-		EXPECT_EQ(from_int[i], 12);
+	{
+		bool matches_expectation = true;
+		for (size_t i = 0; i < from_int.size; ++i) {
+			matches_expectation = matches_expectation && (from_int[i] == 12);
+		}
+		EXPECT_TRUE(matches_expectation);
 	}
 
 	VectorFracSat8::Data data = vec_splat_s8(42);
 	VectorFracSat8 from_data(data);
-	for (size_t i = 0; i < from_data.size; ++i) {
-		EXPECT_EQ(from_data[i], data[i]);
+	{
+		bool matches_expectation = true;
+		for (size_t i = 0; i < from_data.size; ++i) {
+			matches_expectation = matches_expectation && (from_data[i] == data[i]);
+		}
+		EXPECT_TRUE(matches_expectation);
 	}
 }
 
