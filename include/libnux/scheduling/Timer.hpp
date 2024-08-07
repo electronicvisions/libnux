3c67fc510ff69a886029ee335b5b61f5846be837
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2024-08-07 13:41:29 +0200

    chore: Reduce hot-path calculations in scheduling::Timer
    Change-Id: I451d9d9ffe8e4d20a656b716a7fc7aaa1bb32733

--- include/libnux/scheduling/Timer.hpp
+++ include/libnux/scheduling/Timer.hpp
@@ -17,6 +17,7 @@ class Timer
 	time_type m_missed;
 
 	time_type m_last_run;
+	time_type m_end_run;
 
 public:
 	Timer();
@@ -44,6 +45,7 @@ inline Timer::Timer()
 	this->m_num_periods = 0;
 	this->m_period = 0;
 	this->m_last_run = 0;
+	this->m_end_run = 0;
 	this->m_missed = 0;
 }
 
@@ -61,6 +63,8 @@ void Timer::set_service(T& t)
 inline void Timer::set_first_deadline(time_type first)
 {
 	this->m_start = first;
+	m_last_run = first;
+	m_end_run = m_start + m_period * m_num_periods;
 }
 
 inline time_type Timer::get_first_deadline() const
@@ -71,28 +75,26 @@ inline time_type Timer::get_first_deadline() const
 inline void Timer::set_num_periods(time_type num)
 {
 	this->m_num_periods = num;
+	m_end_run = m_start + m_period * m_num_periods;
 }
 
 inline void Timer::set_period(time_type period)
 {
 	this->m_period = period;
+	m_end_run = m_start + m_period * m_num_periods;
 }
 
 inline bool Timer::next_event(Event& event, time_type t)
 {
-	if (t >= this->m_start) {
-		if ((t -= this->m_start) < this->m_period * this->m_num_periods) {
-			if (t >= this->m_last_run) {
-				this->m_last_run += this->m_period;
-				while (this->m_last_run < t) {
-					++this->m_missed;
-					this->m_last_run += this->m_period;
-				}
-				event.id = this->m_id;
-				event.deadline = this->m_last_run + this->m_start;
-				return true;
-			}
+	if ((t >= m_last_run) && (t < m_end_run)) {
+		this->m_last_run += this->m_period;
+		while (this->m_last_run < t) {
+			++this->m_missed;
+			this->m_last_run += this->m_period;
 		}
+		event.id = this->m_id;
+		event.deadline = this->m_last_run;
+		return true;
 	}
 	return false;
 }
